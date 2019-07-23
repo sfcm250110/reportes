4a1155983364c4c9238dc83531caaf48297292de
Santiago Cabrera M. <santiago.cabrera.murillo@gmail.com>
2019-07-23 09:26:10 -0500

    Commit inicial con eclipse

---
+++ src/main/java/com/sc/reporte/almacen/ReporteAlmacenApplication.java
@@ -0,0 +1,13 @@
+package com.sc.reporte.almacen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReporteAlmacenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReporteAlmacenApplication.class, args);
+	}
+
+}
