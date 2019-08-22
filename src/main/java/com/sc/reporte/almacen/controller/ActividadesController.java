f0bb8d59d7845753ffad75babe21e28aa85c7492
Santiago Cabrera M. <santiago.cabrera.murillo@gmail.com>
2019-08-22 00:13:10 -0500

    Ajuste a formulario de actividades

--- src/main/java/com/sc/reporte/almacen/controller/ActividadesController.java
+++ src/main/java/com/sc/reporte/almacen/controller/ActividadesController.java
@@ -8,13 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.ec.reporte.almacen.entity.User;
 import com.sc.reporte.almacen.dto.ChangePasswordForm;
+import com.sc.reporte.almacen.exception.CustomeFieldValidationException;
 import com.sc.reporte.almacen.repository.RoleRepository;
 import com.sc.reporte.almacen.service.UserService;
 
@@ -26,7 +30,7 @@ public class ActividadesController {
 
 	@Autowired
 	RoleRepository roleRepository;
-
+	
 	@GetMapping("/registrar")
 	public String users(Model model) {
 		model.addAttribute("userForm", new User());
@@ -36,6 +40,55 @@ public class ActividadesController {
 		
 		return "actividades/registrar";
 	}
+	
+	@GetMapping("/form")
+    public String showUserForm(Model model){
+        model.addAttribute("usuario", new Usuario());
+        return "userForm";
+    }
+
+    @RequestMapping("/create")
+    public ModelAndView createUser(@Valid Usuario user, BindingResult result) {
+        ModelAndView model = new ModelAndView();
+        model.addObject("usuario", user);
+        model.setViewName(result.hasErrors() ? "userForm" : "userReady"); 
+        return model;
+    }
+	
+	@PostMapping("/userForm")
+	public String createUser(@Valid @ModelAttribute("userForm") User user, BindingResult result, ModelMap model) {
+		if (result.hasErrors()) {
+			model.addAttribute("userForm", user);
+			model.addAttribute("formTab", "active");
+		} else {
+			try {
+				userService.createUser(user);
+				model.addAttribute("userForm", new User());
+				model.addAttribute("listTab", "active");
+
+			} catch (CustomeFieldValidationException cfve) {
+				result.rejectValue(cfve.getFieldName(), null, cfve.getMessage());
+				model.addAttribute("userForm", user);
+				model.addAttribute("formTab", "active");
+				model.addAttribute("userList", userService.getAllUsers());
+				model.addAttribute("roles", roleRepository.findAll());
+			} catch (Exception e) {
+				model.addAttribute("formErrorMessage", e.getMessage());
+				model.addAttribute("userForm", user);
+				model.addAttribute("formTab", "active");
+				model.addAttribute("userList", userService.getAllUsers());
+				model.addAttribute("roles", roleRepository.findAll());
+			}
+		}
+
+		model.addAttribute("userList", userService.getAllUsers());
+		model.addAttribute("roles", roleRepository.findAll());
+		return "user-form/user-view";
+	}
+	
+	
+
+	
 
 	@GetMapping("/consultar")
 	public String consultar(Model model) {
